a9e80fbc4ecd4a4365bf39cb808fbec4be80c146
invierno <1354002980@qq.com>
2020-10-24 18:48:09 +0800

    quickSort

---
+++ quickSort.js
@@ -0,0 +1,18 @@
+let quickSort = arr=>{
+    if(arr.length<=1){return arr}
+    else{
+        let pivotIndex=Math.floor(arr.length/2)
+        let pivot=arr.splice(pivotIndex,1)[0]
+        let left=[]
+        let right=[]
+        for(let i=0;i<arr.length;i++){
+            // if(arr[i]<arr[pivotIndex]){left.push(arr[i])}错误点在于此时arr[pivotIndex]!=pivot
+            
+            if(arr[i]<pivot){left.push(arr[i])}
+            else{
+                right.push(arr[i])
+            }
+        }
+        return quickSort(left).concat([pivot],quickSort(right))
+    }
+}

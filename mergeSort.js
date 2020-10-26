628919fedf6595cb30d35b02da887024d4c509c2
invierno123 <61281923+invierno123@users.noreply.github.com>
2020-10-26 10:26:06 +0800

    加了splice和slice的注释

--- mergeSort.js
+++ mergeSort.js
@@ -1,6 +1,8 @@
 let mergeSort=arr=>{
     if(arr.length===1){return arr}
     let left=arr.slice(0,Math.floor(arr.length/2))//注意拼写啊亲！是slice，不是splice！
+    /*slice(start,end)：方法可从已有数组中返回选定的元素，返回一个新数组，包含从start到end（不包含该元素）的数组元素,end为可选参数，该方法不会改变原数组；
+    splice()：该方法向或者从数组中添加或者删除项目，返回被删除的项目。（该方法会改变原数组），splice（index,howmany,item1,...itemX），item为可选参数，是向数组添加的新项目。*/
     let right=arr.slice(Math.floor(arr.length/2))
     return merge(mergeSort(left),mergeSort(right))
 
